5eb6d5a59dc85638d20381a73f6aec1abd7f8b93
Simon Medina <guevarasimon314@gmail.com>
2024-06-27 18:07:13 -0500

    petwell

---
+++ js/alert.js
@@ -0,0 +1,16 @@
+Swal.fire({
+    title: "Cuida  de tus Mascotas con PetWell, </br> alimentación adecuada, entrenamientobásico, salud y bienestar",
+    width: 600,
+    padding: "2em",
+    color: "black",
+    background: "#fff",
+    confirmButtonColor:'#ff5d34',
+    backdrop: `
+      rgba(47, 47, 47, 0.756)
+      url("img/nyan-cat.gif")
+      left top
+      no-repeat
+    `
+   
+    
+  });
